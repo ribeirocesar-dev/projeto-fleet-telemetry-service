76262d1759de414a221ec9189c4fba6eb8e4345e
cesar822 <costacesar055@gmail.com>
2026-09-08 08:29:09 -0300

    chore(spring): inicializa projeto spring boot
    - Adiciona arquivo pom.xml com dependências iniciais
    - Cria classe principal da aplicação e estrutura de pacotes
    - Inclui classe base de testes
    - Adiciona wrappers do Maven (mvnw e mvnw.cmd)
    - Adiciona application.yaml com configuração inicial

---
+++ src/main/java/com/fleet/telemetry/FleetTelemetryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fleet.telemetry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetTelemetryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleetTelemetryServiceApplication.class, args);
+	}
+
+}
